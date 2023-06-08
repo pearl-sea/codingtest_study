a038b7c1808e09b6291d1e1d4b61abe0bccef468
pearl-sea <sakanaplanet@gmail.com>
2023-06-08 17:11:45 +0900

    알고리즘의 수행 시간 2

---
+++ 24263.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('./input.txt').toString();
+
+console.log(input)
+
+//이 문제는 답을 내는것보다 개념을 이해하고 정리하는게 더 중요하기 때문에
+//공부내용을 적은 블로그 게시글 링크를 주석으로 첨부합니다.
+
+//https://www.notion.so/sea-pearl/1-O-1-3446edae1fde4fe788cdf145c5f9df88?pvs=4
