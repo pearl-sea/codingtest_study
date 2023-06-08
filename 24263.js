f1f7cb29505b9cb62e4d001a6647a4189db67b9f
pearl <102569322+pearl-sea@users.noreply.github.com>
2023-06-08 23:23:32 +0900

    알고리즘의 수행 시간 2

--- 24263.js
+++ 24263.js
@@ -1,6 +1,7 @@
 const input = require('fs').readFileSync('./input.txt').toString();
 
 console.log(input)
+console.log(1)
 
 //이 문제는 답을 내는것보다 개념을 이해하고 정리하는게 더 중요하기 때문에
 //공부내용을 적은 블로그 게시글 링크를 주석으로 첨부합니다.
