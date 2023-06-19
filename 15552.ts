189649c0b6eb0a00e86620a9b69349fa015cc6b5
pearl-sea <sakanaplanet@gmail.com>
2023-06-19 12:00:58 +0900

    빠른 A+B

---
+++ 15552.ts
@@ -0,0 +1,22 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('./input.txt').toString().trim().split('\r\n');
+const arr: string[] = input.slice(1);
+
+let output: string = '';
+
+for (let el of arr) {
+  const a = Number(el.split(' ')[0]);
+  const b = Number(el.split(' ')[1]);
+
+  output += a + b + ',';
+}
+
+console.log(
+  output
+    .split(',')
+    .join('\n')
+    .slice(0, output.length - 1)
+);
+
+//큐2 를 풀면서 시간초과가 났고 그 원인을 해결하는데 도움을 주었던 문제
